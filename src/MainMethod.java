429ff9b3db0e51c24c59f27c3f4635ae8210b5c6
MarioARC <mardiamond10@outlook.com>
2022-02-14 08:08:02 -0800

    Added RegExId identity2

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -5,13 +5,17 @@ public class MainMethod {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         RegExID identity1 = new RegExID();
+        RegExID identity2 = new RegExID();
 
         identity1.setFirstName("");
         identity1.setLastName("");
         identity1.setIdNumber("");
         identity1.setPassword("");
 
+
+
         System.out.println(identity1);
+        System.out.println(identity2);
 
     }
 }
